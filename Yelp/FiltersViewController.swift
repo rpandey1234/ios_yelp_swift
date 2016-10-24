4b6f4a5a8b1bb34a954aed111347cb50debaa294
Rahul Pandey <rahul@pinterest.com>
2016-10-23 21:48:33 -0700

    use switch statements

--- Yelp/FiltersViewController.swift
+++ Yelp/FiltersViewController.swift
@@ -14,6 +14,12 @@ import UIKit
 
 class FiltersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, SwitchCellDelegate {
 
+    let METERS_IN_MILE: Int = 1609
+    let DEAL_SECTION = 0
+    let DISTANCE_SECTION = 1
+    let SORT_SECTION = 2
+    let CATEGORY_SECTION = 3
+
     @IBOutlet weak var tableView: UITableView!
     var categories: [[String: String]]!
     weak var delegate: FiltersViewControllerDelegate?
@@ -32,22 +38,24 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
         
         // Categories
         var selectedCategories = [String]()
-        for (row, isSelected) in switchStates[2]! {
-            if isSelected {
-                selectedCategories.append(categories[row]["code"]!)
+        if let categorySelection = switchStates[CATEGORY_SECTION] {
+            for (row, isSelected) in categorySelection {
+                if isSelected {
+                    selectedCategories.append(categories[row]["code"]!)
+                }
             }
         }
         if selectedCategories.count > 0 {
             filters["categories"] = selectedCategories as AnyObject?
         }
         // Offering a deal
-        if switchStates[0]?[0] != nil {
-            filters["offeringDeal"] = switchStates[0]?[0] as AnyObject?
+        if switchStates[DEAL_SECTION]?[0] != nil {
+            filters["offeringDeal"] = switchStates[DEAL_SECTION]?[0] as AnyObject?
         }
         
         // Sort criteria
         var sortCriteria: YelpSortMode?
-        for (row, isSelected) in switchStates[2]! {
+        for (row, isSelected) in switchStates[SORT_SECTION]! {
             if isSelected {
                 sortCriteria = sortCriteriaData[row].1
                 break
@@ -57,9 +65,9 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
             filters["sortCriteria"] = sortCriteria as AnyObject?
         }
         
-        // Distance (should be radius_filter in meters)
+        // Distance (in meters)
         var distance: Int?
-        for (row, isSelected) in switchStates[1]! {
+        for (row, isSelected) in switchStates[DISTANCE_SECTION]! {
             if isSelected {
                 distance = distanceData[row].1
                 break
@@ -77,8 +85,16 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
         tableView.dataSource = self
         tableView.delegate = self
         categories = yelpCategories()
-        sortCriteriaData = [("Best Match", YelpSortMode.bestMatched), ("Distance", YelpSortMode.distance), ("Highly rated", YelpSortMode.highestRated)]
-        distanceData = [("Auto", nil), ("0.3 miles", Int(1609 * 0.3)), ("1 mile", 1609), ("5 miles", 1609 * 5), ("20 miles", 1609 * 20)]
+        sortCriteriaData = [
+            ("Best Match", YelpSortMode.bestMatched),
+            ("Distance", YelpSortMode.distance),
+            ("Highly rated", YelpSortMode.highestRated)]
+        distanceData = [
+            ("Auto", nil),
+            ("0.3 miles", Int(Double(self.METERS_IN_MILE) * 0.3)),
+            ("1 mile", self.METERS_IN_MILE),
+            ("5 miles", self.METERS_IN_MILE * 5),
+            ("20 miles", self.METERS_IN_MILE * 20)]
     }
 
     override func didReceiveMemoryWarning() {
@@ -91,29 +107,33 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 {
+        switch section {
+        case DEAL_SECTION:
             return "offering a deal"
-        } else if section == 1 {
+        case DISTANCE_SECTION:
             return "distance"
-        } else if section == 2 {
+        case SORT_SECTION:
             return "sort by"
-        } else if section == 3 {
+        case CATEGORY_SECTION:
             return "category"
+        default:
+            return "not found"
         }
-        return "not found"
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
+        switch section {
+        case DEAL_SECTION:
             return 1
-        } else if section == 1 {
+        case DISTANCE_SECTION:
             return distanceData.count
-        } else if section == 2 {
+        case SORT_SECTION:
             return sortCriteriaData.count
-        } else if section == 3 {
+        case CATEGORY_SECTION:
             return categories.count
+        default:
+            return 1
         }
-        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -124,22 +144,25 @@ class FiltersViewController: UIViewController, UITableViewDataSource, UITableVie
         }
         print("section \(indexPath.section): \(indexPath.row)")
         cell.onSwitch.isOn = switchStates[indexPath.section]?[indexPath.row] ?? false
-        if indexPath.section == 0 {
-            cell.switchLabel.text = "Offering a Deal"
-        } else if indexPath.section == 1 {
-            cell.switchLabel.text = distanceData[indexPath.row].0
-        } else if indexPath.section == 2 {
-            cell.switchLabel.text = sortCriteriaData[indexPath.row].0
-        } else {
-            cell.switchLabel.text = categories[indexPath.row]["name"]
+        var text: String?
+        switch indexPath.section {
+        case DEAL_SECTION:
+            text = "Offering a Deal"
+        case DISTANCE_SECTION:
+            text = distanceData[indexPath.row].0
+        case SORT_SECTION:
+            text = sortCriteriaData[indexPath.row].0
+        default:
+            text = categories[indexPath.row]["name"]
         }
+        cell.switchLabel.text = text
         return cell
     }
     
     func switchCell(switchCell: SwitchCell, didChange value: Bool) {
         let indexPath = tableView.indexPath(for: switchCell)
         switchStates[(indexPath?.section)!]?[(indexPath?.row)!] = value
-        if indexPath?.section == 1 || indexPath?.section == 2 {
+        if indexPath?.section == DISTANCE_SECTION || indexPath?.section == SORT_SECTION {
             if value {
                 let numberRows = tableView.numberOfRows(inSection: (indexPath?.section)!)
                 // Toggle all the others in this section
