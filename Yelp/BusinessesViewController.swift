31664ff485d0badcd883954a32ea106feff196fb
Rahul Pandey <rahul@pinterest.com>
2016-10-23 22:47:24 -0700

    infinite scroll

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, FiltersViewControllerDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UIScrollViewDelegate, FiltersViewControllerDelegate {
     
-    static let RestaurantQuery = "restuarants"
+    static let RestaurantQuery = "restaurants"
     var businesses: [Business]!
     var filteredBusinesses: [Business]!
+    var isMoreDataLoading = false
+    var numShown = 0
+    var currentFilters: [String : AnyObject] = [:]
+    var loadingMoreView: InfiniteScrollActivityView?
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -27,11 +31,18 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         searchBar.delegate = self
         searchBar.sizeToFit()
         navigationItem.titleView = searchBar
-        Business.searchWithTerm(term: "restaurants", completion: { (businesses: [Business]?, error: Error?) -> Void in
-            self.businesses = businesses
-            self.filteredBusinesses = businesses
-            self.tableView.reloadData()
-        })
+        
+        // Set up Infinite Scroll loading indicator
+        let frame = CGRect(x: 0, y: tableView.contentSize.height, width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight)
+        loadingMoreView = InfiniteScrollActivityView(frame: frame)
+        loadingMoreView!.isHidden = true
+        tableView.addSubview(loadingMoreView!)
+        
+        var insets = tableView.contentInset;
+        insets.bottom += InfiniteScrollActivityView.defaultHeight;
+        tableView.contentInset = insets
+        
+        loadData(offset: numShown)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -45,16 +56,37 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         tableView.reloadData()
     }
     
-    func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
-        let categories = filters["categories"] as! [String]?
-        let deals = filters["offeringDeal"] as! Bool?
-        let sortCriteria = filters["sortCriteria"] as! YelpSortMode?
-        let distance = filters["distance"] as! Int?
-        Business.searchWithTerm(term: BusinessesViewController.RestaurantQuery, sort: sortCriteria, categories: categories, deals: deals, distance: distance) { (businesses: [Business]?, error: Error?) -> Void in
-            self.businesses = businesses
-            self.filteredBusinesses = self.businesses
+    func loadData(offset: Int) {
+        let categories = currentFilters["categories"] as! [String]?
+        let deals = currentFilters["offeringDeal"] as! Bool?
+        let sortCriteria = currentFilters["sortCriteria"] as! YelpSortMode?
+        let distance = currentFilters["distance"] as! Int?
+        
+        Business.searchWithTerm(term: BusinessesViewController.RestaurantQuery, sort: sortCriteria, categories: categories, deals: deals, distance: distance, offset: offset) { (businesses: [Business]?, error: Error?) -> Void in
+            if offset == 0 {
+                self.businesses = businesses
+                self.filteredBusinesses = self.businesses
+                self.numShown = self.filteredBusinesses.count
+            } else {
+                if let businesses = businesses {
+                    // TODO: find better way to append [Business]! and [Business]
+                    for business in businesses {
+                        self.businesses.append(business)
+                    }
+                }
+                self.filteredBusinesses = self.businesses
+                self.numShown += self.filteredBusinesses.count
+            }
             self.tableView.reloadData()
+            self.isMoreDataLoading = false
+            self.loadingMoreView!.stopAnimating()
         }
+    }
+    
+    func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
+        currentFilters = filters
+        numShown = 0
+        loadData(offset: numShown)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,6 +105,24 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         // Dispose of any resources that can be recreated.
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if !isMoreDataLoading {
+            // Calculate position of one screen length before the bottom of results
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            if scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging {
+                isMoreDataLoading = true
+                // Update position of loadingMoreView, and start loading indicator
+                let frame = CGRect(x: 0, y: tableView.contentSize.height, width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight)
+                loadingMoreView?.frame = frame
+                loadingMoreView!.startAnimating()
+                
+                // Code to load more results
+                loadData(offset: numShown)
+            }
+            
+        }
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let navigationController = segue.destination as! UINavigationController
